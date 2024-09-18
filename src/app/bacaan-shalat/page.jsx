b2dfec4a2c4d82f25d214132290f771e84f9bdd9
dimasbp17 <dimas.prasetyo@students.amikom.ac.id>
2024-09-18 17:40:41 +0700

    add:bacaan shalat

--- src/app/bacaan-shalat/page.jsx
+++ src/app/bacaan-shalat/page.jsx
@@ -1,9 +1,19 @@
+'use client';
+
+import { Card } from '@material-tailwind/react';
 import React from 'react';
 
 const BacaanShalat = () => {
   return (
     <>
-      <div>dada</div>
+      <div className="md:px-20">
+        <Card className="p-3 my-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+          nisi ipsa eos accusantium aliquid suscipit, quod ducimus, ea minima
+          quidem fuga saepe accusamus harum ratione. Incidunt quos non numquam
+          enim.
+        </Card>
+      </div>
     </>
   );
 };
