a406d3a30bc8d8c21ec61d8fe72ad84434b03ce2
dimasbp17 <dimas.prasetyo@students.amikom.ac.id>
2024-09-24 20:12:17 +0700

    update:navbar, surah

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import Search from './Search';
 const Navbar = () => {
   return (
     <>
-      <div className="flex items-center justify-between w-full px-20 py-8 text-white bg-[#121928]">
+      <div className="flex items-center justify-between w-full px-4 lg:px-20 py-8 text-white bg-[#121928]">
         <Link href={'/'}>
           <div className="text-2xl font-bold">E-Alqur&apos;an</div>
         </Link>
